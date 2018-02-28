1b4944b31455136956cccbd1896ed7d8e1ea14fa
Abhishek Chadha <abhishekchadha235@gmail.com>
2018-02-28 14:47:23 -0500

    fix app.js and start scripts

--- app.js
+++ app.js
@@ -1,40 +1,14 @@
-var port = process.env.PORT || 3000,
-    http = require('http'),
-    fs = require('fs');
+const path = require('path');
 
-var app = http.createServer(function (req, res) {
-  if (req.url.indexOf('/img') != -1) {
-    var filePath = req.url.split('/img')[1];
-    fs.readFile(__dirname + '/public/img' + filePath, function (err, data) {
-      if (err) console.log(err);
-      res.writeHead(200, {'Content-Type': 'image/svg+xml'});
-      res.write(data);
-      res.end();
-    });
-  } else if (req.url.indexOf('/js') != -1) {
-    var filePath = req.url.split('/js')[1];
-    fs.readFile(__dirname + '/public/js' + filePath, function (err, data) {
-      if (err) console.log(err);
-      res.writeHead(200, {'Content-Type': 'text/javascript'});
-      res.write(data);
-      res.end();
-    });
-  } else if(req.url.indexOf('/css') != -1) {
-    var filePath = req.url.split('/css')[1];
-    fs.readFile(__dirname + '/public/css' + filePath, function (err, data) {
-      if (err) console.log(err);
-      res.writeHead(200, {'Content-Type': 'text/css'});
-      res.write(data);
-      res.end();
-    });
-  } else {
-    fs.readFile(__dirname + '/public/index.html', function (err, data) {
-      if (err) console.log(err);
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-}).listen(port, '0.0.0.0');
+const Botpress = require(path.join(__dirname, 'node_modules/botpress/lib/node.bundle.js')).Botpress()
+const projectPath = path.resolve(__dirname)
+const botfilePath = path.join(projectPath, 'botfile.js');
+const botfile = require(botfilePath);
+const bot = new Botpress({botfile: botfilePath});
 
-module.exports = app;
+function startBot () {
+  const res = bot.start();
+}
+
+startBot();
+
