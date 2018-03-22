4d482fefd1cb2c52cada8ea814b979a1a4089d34
Abhishek Chadha <abhishekchadha235@gmail.com>
2018-03-21 20:51:34 -0400

    allow templating of content.yml

--- src/setup.js
+++ src/setup.js
@@ -2,22 +2,57 @@ const fs = require('fs');
 const path = require('path');
 const menu = require('./menu');
 const YAML = require('json2yaml');
-
+const postbacks = require('./postbacks');
 const _ = require('lodash');
+
 const templateFileName = 'messenger.config.template.yml';
 const botpressMessengerConfigName = 'botpress-messenger.config.yml';
 
-const config = require('./config');
+
+const contentTemplateFileName = 'content.template.yml';
+const botpressContentFileName = 'content.yml';
+
+
 const messages = require('./messages');
+const botfile = require('../botfile');
 
 // Creates botpress-messenger.config.yml
-module.exports = function setup() {
-    const templateFileContents = fs.readFileSync(path.join(__dirname, templateFileName));
-    const ymlTemplate = _.template(templateFileContents);
-    const ymlText = YAML.stringify(menu).replace('---\n', '');
-    const str = ymlTemplate(Object.assign({}, config, {
+async function setup() {
+    const config = await require('./config');
+    const express = require('express');
+    const app = express();
+
+    // Setup messenger configuration YML
+    const menuYMLText = YAML.stringify(menu).replace('---\n', '');
+    const messengerConfigVars = Object.assign({}, config, {
         greetingMessage: messages.greeting,
-        menu: ymlText
-    }))
-    fs.writeFileSync(path.join(__dirname, '..', botpressMessengerConfigName), str);
+        menu: menuYMLText
+    });
+    templateYaml(
+        path.join(__dirname, templateFileName),
+        path.join(__dirname, '..', botpressMessengerConfigName),
+        messengerConfigVars
+    );
+    
+    // Setup content YML
+    templateYaml(
+        path.join(__dirname, contentTemplateFileName),
+        path.join(__dirname, '..', botpressContentFileName),
+        {
+            postbacks: postbacks,
+            staticUrl: config.staticUrl
+        }
+    );
+
+    // Serve static directory
+    app.use(express.static(path.join(__dirname, 'static')));
 }
+
+function templateYaml(templateFilePath, targetFilePath, variables) {
+    const templateFileContents = fs.readFileSync(templateFilePath);
+    const ymlTemplate = _.template(templateFileContents);
+    const str = ymlTemplate(variables);
+    fs.writeFileSync(targetFilePath, str);
+}
+
+module.exports = setup;
