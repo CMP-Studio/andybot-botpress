00f10f1f234ebc4b40f6fd6a78cbb71b67e6d409
Abhishek Chadha <abhishekchadha235@gmail.com>
2018-06-04 13:39:41 -0400

    delete unnecessary files

--- src/messages.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    greeting: "I'll be your guide to Summer Adventure at the Carnegie Museums of Pittsburgh. I'll help you earn stamps for visiting museums and attending special events. Each stamp increases your chance to win at the End-Of-Summer celebration."
-};
