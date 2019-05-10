33da8cb77311bbab47487d9805df0ef9570165d8
Sam Ticknor <ticknors@carnegiemuseums.org>
2019-05-10 14:08:44 -0400

    Fixing bugs

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ module.exports = function (bp) {
 		const cleared = await andybot.scavengerhunt.clearProgress(pageId);
 		event.reply("#scavengerhunt-clearhunt");
 		try {
-			scavengerHuntHandler(convo, event, activityName);
+			activityHandlers.scavengerhunt(convo, event, activityName);
 		} catch (err) {
 			console.error(err);
 		}
